6a813d527c5d2799e58c0f8fe81ef85886e711f8
Ajay Singh Rawat <149299603+AjaySRawat07@users.noreply.github.com>
2024-05-10 22:29:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
-import Planner from './components/EducatorPlanner/planner'
+// import Planner from './components/EducatorPlanner/planner'
+import Generator from './components/LoremGenerator/LoremFile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Planner />
+{/*     <Planner /> */}
+    <Generator />
   </React.StrictMode>,
 )
